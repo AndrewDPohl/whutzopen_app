095ac15abaea43f35432773c180363cc64889629
Andrew Pohl <andrewpohl@Andrews-MacBook-Pro-2.local>
2014-12-15 12:41:10 -0800

    added public file with stylesheets/bootstrap

--- app.js
+++ app.js
@@ -3,13 +3,16 @@ var express = require("express"),
   db = require("./models"),
   app = express();
 
+app.use(express.static(__dirname + "/public"));
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({extended: true}));
 
- db.sequelize.sync().then(function() {
-    var server = app.listen(3000, function() {
-    console.log(new Array(51).join("*"));
-    console.log("\t LISTENING ON: \n\t\t localhost:3000");
-    console.log(new Array(51).join("*")); 
-  });
+app.get("/", function (req, res){
+  res.render("sites/home");
+});
+
+app.listen(3000, function() {
+  console.log(new Array(51).join("*"));
+  console.log("\t LISTENING ON: \n\t\t localhost:3000");
+  console.log(new Array(51).join("*")); 
 });
