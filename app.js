0cac981bf8bd6da0a1476a819e9863b991b85fb3
Andrew Pohl <andrewpohl@Andrews-MacBook-Pro-2.local>
2014-12-15 12:28:05 -0800

    setting up simple app.js

--- app.js
+++ app.js
@@ -6,10 +6,13 @@ var express = require("express"),
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({extended: true}));
 
- db.sequelize.sync().then(function() {
-    var server = app.listen(3000, function() {
-    console.log(new Array(51).join("*"));
-    console.log("\t LISTENING ON: \n\t\t localhost:3000");
-    console.log(new Array(51).join("*")); 
-  });
+app.get("/", function (req, res){
+  res.send("HELLO WORLD");
+});
+
+
+app.listen(3000, function() {
+  console.log(new Array(51).join("*"));
+  console.log("\t LISTENING ON: \n\t\t localhost:3000");
+  console.log(new Array(51).join("*")); 
 });
