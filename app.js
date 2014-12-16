9d7160da6f740ad001f0c579016287cff9ec4750
Andrew Pohl <andrewpohl@Andrews-MacBook-Pro-2.local>
2014-12-15 18:34:27 -0800

    latest updates to whutzup app

--- app.js
+++ app.js
@@ -1,17 +1,123 @@
 var express = require("express"),
   bodyParser = require("body-parser"),
   db = require("./models"),
-  app = express();
+  app = express(),
+  passport = require("passport"),
+  session = require("cookie-session"),
+  bcrypt = require("bcrypt");
 
 app.use(express.static(__dirname + "/public"));
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({extended: true}));
 
-app.get("/", function (req, res){
-  res.render("sites/home");
+app.use(session( {
+  secret:'thisismysecretkey',
+  name: 'oatmeal-raisen',
+  madage: 3600000
+}));
+//get passport started
+app.use(passport.initialize());
+app.use(passport.session());
+//SERIALIZE USER DATA
+passport.serializeUser(function(user, done){
+  console.log("SERIALIZED JUST RAN!");
+  done(null, user.id);
+});
+//DESERIALIZE
+passport.deserializeUser(function(id, done){
+  console.log("DESERIALIZED JUST RAN!");
+  db.user.find({
+    where: {
+      id: id
+    }
+  })
+  .then(function(user){
+    done(null, user);
+  },
+  function(err) {
+    done(err, null);
+  });
+}); 
+
+app.get("/", function (req, res) {
+  console.log(req.get("cookie"));
+  var rawValue = req.get("cookie")
+  var count = parseInt(rawValue) || 0;
+  count += 1;
+  res.set("Set-Cookie", count)
+  res.send("hello " + count);
 });
 
 
+//Show the home page/login page
+app.get("/home", function (req, res){
+  res.render("sites/home");
+});
+
+//WHEN SOMEONE WANTS TO SUBMIT A SIGN UP/LOGIN
+app.post("/home", function (req, res) {
+  console.log("POST /home");
+  var newUser = req.body.user;
+  console.log("New User:", newUser);
+  // CREATE a user and secure their password
+  db.users.createSecure(newUser.email, newUser.password, newUser.firstName, newUser.lastName, 
+    function () {
+      // if a user fails to create make them signup again
+      res.redirect("/sign_up");
+    },
+    function (err, user) {
+      // when successfully created log the user in
+      // req.login is given by the passport
+      req.login(user, function(){
+        // after login redirect show page
+        console.log("Id: ", user.id)
+        res.redirect('/home/' + user.id);
+      });
+    })
+});
+
+app.get("/users/:id", function (req, res) {
+  var id = req.params.id;
+  db.user.find(id)
+    .then(function (user) {
+      res.render("users/show", {user: user});
+    })
+    .error(function () {
+      res.redirect("/sign_up");
+    })
+});
+
+
+// When someone wants the login page
+app.get("/login", function (req, res) {
+  res.render("sites/login");
+});
+
+// Authenticating a user
+app.post('/login', passport.authenticate('local', {
+  successRedirect: '/',
+  failureRedirect: '/login'
+}));
+
+
+app.get("/", function (req, res) {
+  console.log(req.user)
+  // req.user is the user currently logged in
+
+  if (req.user) {
+    res.render("site/index", {user: req.user});
+  } else {
+    res.render("site/index", {user: false});
+  }
+});
+
+app.get("/index", function (req, res){
+  res.render("sites/index");
+});
+
+app.get("/show", function (req, res){
+  res.render("sites/show");
+});
 
 
 app.listen(3000, function() {
